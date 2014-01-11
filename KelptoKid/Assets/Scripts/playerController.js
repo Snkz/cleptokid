660b146a6b15c28f24fc26053bbb1da361a802d2
Abdi <a.dahir@utoronto.ca>
2014-01-11 14:38:47 -0500

    Added player controls
    fixed drag and acceleartation

--- KelptoKid/Assets/Scripts/playerController.js
+++ KelptoKid/Assets/Scripts/playerController.js
@@ -1,29 +1,32 @@
 #pragma strict
 
-var movement:int;
+var force = 100;
+var start_x = 0;
+var start_y = 0;
 private var lastDirectionFaced:String=("Right");
 
 function Start () {
-transform.position=Vector2(0,0);
+	transform.position=Vector2(start_x, start_y);
 }
+
 function FixedUpdate () {	
 	if(Input.GetKey(KeyCode.A)){
-		rigidbody2D.AddForce(Vector2(-2f,0));
+		rigidbody2D.AddForce(Vector2(-1*force, 0));
 		transform.localScale.x=-1;
 		lastDirectionFaced=("Left");
 	}
 	if(Input.GetKey(KeyCode.D)){
-		rigidbody2D.AddForce(Vector2(2f,0));
+		rigidbody2D.AddForce (Vector2(1*force, 0));
 		transform.localScale.x=1;
 		lastDirectionFaced=("Right");
 	}
 	if(Input.GetKey(KeyCode.W)){
-		rigidbody2D.AddForce(Vector2(0,2f));
+		rigidbody2D.AddForce(Vector2(0, 1*force));
 		//transform.localScale.y=-1;
 		lastDirectionFaced=("Up");
 	}
 	if(Input.GetKey(KeyCode.S)){
-		rigidbody2D.AddForce(Vector2(0,-2f));
+		rigidbody2D.AddForce(Vector2(0, -1*force));
 		//transform.localScale.y=-1;
 		lastDirectionFaced=("Down");
 	}
